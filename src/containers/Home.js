ae22d110c0bc6f7df08433c566fc5fb4051914aa
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-13 16:29:06 -0300

    Home & Not Found page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles/Home.css';
+import confLogo from '../images/platziconf-logo.svg';
+import astronauts from '../images/astronauts.svg';
 
 class Home extends Component {
   render() {
-    return <h1>home</h1>;
+    return (
+      <div className="full__container">
+        <div className="container">
+          <div className="Home__content">
+            <div className="Home__left-box">
+              <img src={confLogo} alt="Conf Logo" />
+              <h1 className="pt-2">Print your badges</h1>
+              <p className="pt-1 Home__subtitle">
+                The easiest way to manage your conference
+              </p>
+              <Link to="/badges" className="btn btn-primary">
+                Start now
+              </Link>
+            </div>
+            <div className="Home__right-box">
+              <img src={astronauts} alt="Astronauts" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import astronaut from '../images/astronaut.webp';
 class NotFound extends Component {
   render() {
-    return <h1>hola</h1>;
+    return (
+      <div className="full__container">
+        <img src={astronaut} alt="Image" className="img-fluid" />
+
+        <h2 className="pt-5">Ups! No se ha encontrado la página solicitada</h2>
+      </div>
+    );
   }
 }
 
