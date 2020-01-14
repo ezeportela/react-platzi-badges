4440dc805724fafe9728c76004e215aa4ab912cc
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-14 12:24:40 -0300

    Fix layout

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -5,7 +5,11 @@ class NotFound extends Component {
   render() {
     return (
       <div className="full__container">
-        <img src={astronaut} alt="astronaut" className="img-fluid" />
+        <img
+          src={astronaut}
+          alt="astronaut"
+          className="img-fluid resize-image"
+        />
 
         <h2 className="pt-5">Ups! No se ha encontrado la página solicitada</h2>
       </div>
